31b428ae5e77d2f08c2d80d1e946c8c731b618a8
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-09-09 20:56:55 +0000

    customized site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   // Add your domain here
-   site: 'https://lexingtonthemes.com',
+   site: 'https://onelabs.one',
   integrations: [ sitemap()]
 });
